82bf135008aaaa02c2a5e201f49d65034d772495
tiends03 <141436969+tiends03@users.noreply.github.com>
2023-10-13 22:49:46 +0700

    menus(create,update,delete,....)

--- OrderEats/app/Models/Menus.php
+++ OrderEats/app/Models/Menus.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menus extends Model
 {
-    //
+    protected $fillable = ['item_name','description','price'];
+
+    protected $table = 'menus';
+
+    public function getAllPaginated() {
+        return $this->paginate(2); // Phân trang với 10 phần tử trên mỗi trang
+    }
+
 }
