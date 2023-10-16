77cc67f5adf4069266e27b9a13a9778a339b1ba6
linhtd2805 <134181016+linhtd2805@users.noreply.github.com>
2023-10-16 17:12:28 +0700

    2

--- OrderEats/app/Http/Controllers/FirebaseController.php
+++ OrderEats/app/Http/Controllers/FirebaseController.php
@@ -9,23 +9,21 @@ use Kreait\Firebase\Messaging\Notification;
 class FirebaseController extends Controller
 {
     public function sendNotification()
-    {
+    {   
         $token = 'cE3_2LWwQA2KbCM7N9HhvX:APA91bF-yAHHu7BibgXZMeosQVrvEg5d-bx4OWNyW5qBHhnRKXLfDfELqkgEAkdBcjcySiW30mVjXmC9ySdzvK1AZRVJ4zKSZNuHW87v16B3LINQf60l5Wmfmjd5_7lP4mftUBJqqnyP';
+        $notificationData = [
+            'title' => 'Test Notification',
+            'body' => 'This is a test notification from Lumen.',
+        ];
 
-$notificationData = [
-    'title' => 'Test Notification',
-    'body' => 'This is a test notification from Lumen.',
-];
+        $notification = Notification::fromArray($notificationData);
+        $message = CloudMessage::new()
+            ->withNotification($notification)
+            ->withTarget('token', $token);
 
-$notification = Notification::fromArray($notificationData);
+        app('firebase.messaging')->send($message);
 
-$message = CloudMessage::new()
-    ->withNotification($notification)
-    ->withTarget('token', $token);
-
-app('firebase.messaging')->send($message);
-
-return 'Notification sent!';
+        return 'Notification sent!';
     }
 
 }
