707953f4553c2d99a73497160178abb31c27e63f
linhtd2805 <134181016+linhtd2805@users.noreply.github.com>
2023-11-01 15:53:07 +0700

    1

--- OrderEats/app/Http/Controllers/ShipperCheckOrderController.php
+++ OrderEats/app/Http/Controllers/ShipperCheckOrderController.php
@@ -149,7 +149,6 @@ class ShipperCheckOrderController extends Controller
         
         if ($orders->order_status === 'Đang xử lý' && $request->input('order_status') === 'Đang giao') {
             $orders->order_status = $request->input('order_status');
-<<<<<<< HEAD
             $fcm = new FirebaseController();
             $name = $orders->users->fullname;
             $result = $fcm ->sendNotification($orders->user_id, "Trạng Thái Hàng '$name'", "Đơn hàng của bạn đang giao");
@@ -159,14 +158,12 @@ class ShipperCheckOrderController extends Controller
             $fcm = new FirebaseController();
             $name = $orders->users->fullname;
             $result = $fcm ->sendNotification($orders->user_id, "Trạng Thái Hàng '$name'", "Đơn hàng của bạn đã bị hủy");
-=======
             $this->createActivityLog(auth()->user()->id, 'Đang giao');
 
         } elseif ($orders->order_status === 'Đang giao' && $request->input('order_status') === 'Hủy bỏ') {
             $orders->order_status = $request->input('order_status');
             $this->createActivityLog(auth()->user()->id, 'Hủy đơn hàng');
             
->>>>>>> d675f3cae5b87997ece01d313543704d5ff08de6
         } else {
             return response()->json(['error' => 'Bạn chỉ có thể xác nhận đơn hàng hoặc có thể hủy bỏ đơn hàng đang giao'], 403);
         }
