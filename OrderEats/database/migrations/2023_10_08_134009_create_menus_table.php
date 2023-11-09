88b220e83980877be33b695ed49db8603bed3f9e
tranminhhai0727 <134531531+tranminhhai0727@users.noreply.github.com>
2023-11-09 15:39:45 +0700

    asas

--- OrderEats/database/migrations/2023_10_08_134009_create_menus_table.php
+++ OrderEats/database/migrations/2023_10_08_134009_create_menus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMenuesTable extends Migration
+class CreateMenusTable extends Migration
 {
     /**
      * Run the migrations.
