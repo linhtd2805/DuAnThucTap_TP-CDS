bdaf042235349db91731b70bca97320e03711790
tranminhhai0727 <134531531+tranminhhai0727@users.noreply.github.com>
2023-11-09 15:07:41 +0700

    them trangthai

--- OrderEats/database/migrations/2023_10_08_134010_orders.php
+++ OrderEats/database/migrations/2023_10_08_134010_orders.php
@@ -20,7 +20,7 @@ class Orders extends Migration
             $table->unsignedBigInteger('menu_id');
             $table->integer('quantity');
             $table->decimal('total_price', 8, 2);
-            $table->enum('order_status', ['Đang xử lý', 'Đã giao', 'Hủy bỏ', 'Đang giao']);
+            $table->enum('order_status', ['Đang xử lý', 'Đã giao', 'Hủy bỏ', 'Đang giao','Đã xác nhận']);
             $table->timestamps();
     
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
